2056faf21426871ac4c8bb412bb8d6e2619905ae
SatwikWalke <147227971+SatwikWalke@users.noreply.github.com>
2024-11-20 23:25:17 +0530

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# config.py
+
+INPUT_IMAGE_PATH = r"C:\Users\manik\Documents\Background Replacement using Segmentation Mask\images\satwik.jpg"
+BACKGROUND_IMAGE_PATH = r"C:\Users\manik\Documents\Background Replacement using Segmentation Mask\images\gwenbg.jpg"
+OUTPUT_IMAGE_PATH = r"C:\Users\manik\Documents\Background Replacement using Segmentation Mask\output\output_with_new_background.jpg"  # Ensure file name and extension
